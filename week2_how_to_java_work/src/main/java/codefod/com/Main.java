6b2e766710adb27647819735d3b8c1a22eca46d4
hantn127 <hanntn072003@gmail.com>
2025-02-22 23:18:56 +0700

    Answer for assignment week 2

--- week2_how_to_java_work/src/main/java/codefod/com/Main.java
+++ week2_how_to_java_work/src/main/java/codefod/com/Main.java
@@ -1,10 +1,30 @@
 package codefod.com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         // TODO: Viết một chương trình Java cơ bản và thực chạy compile file chương trình ra bytecode
         // TODO: Thực chạy file bytecode bằng lệnh java
         // TODO: Thực hiện chạy file bytecode với cấu hình heap size, gc, ...
+
+        List<Object> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000000; i++) {
+            list.add(new Object());
+        }
+        System.out.println("Add " + list.size() + " into lists.");
+
+        System.gc();
+
+        for (int i = 0; i < 1000000; i++) {
+            new Object();
+        }
+
+        System.gc();
+
+        System.out.println("Program completed.");
     }
 }
